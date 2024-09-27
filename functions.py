903db235d90121f368a11e6f1f117a6a7c75c53a
yannick <yannick.baldewijns@outlook.be>
2024-09-27 09:06:42 +0200

    Create a basic GUI without interactions, day 16

--- functions.py
+++ functions.py
@@ -1,17 +1,17 @@
-filepath_1 = ("/Users/yannick/Library/CloudStorage/OneDrive-Persoonlijk/ISSO/Courses/Python zelfstudie/"
+FILEPATH_1 = ("/Users/yannick/Library/CloudStorage/OneDrive-Persoonlijk/ISSO/Courses/Python zelfstudie/"
               "Python_Mega_Course_60_Days/App 1/pythonProject/todos.txt")
 
 
 def get_todos():
     """Read a text file and return the to-do items."""
-    with open(filepath_1, "r") as file_local:
+    with open(FILEPATH_1, "r") as file_local:
         todos_local = file_local.readlines()
         return todos_local
 
 
 def store_todos(todos_arg):
     """Take a to-do list and store them in a text file."""
-    with open(filepath_1, "w") as file:
+    with open(FILEPATH_1, "w") as file:
         file.writelines(todos_arg)
 
 
